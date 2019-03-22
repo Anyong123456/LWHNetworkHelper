e7e525db18e13fa69277e416fb327474781c3c00
liwenhan <wh.li@muheda.com>
2019-03-22 10:43:35 +0800

    first commit

---
+++ LWHNetworkHelper/LWHNetworkHelper.h
@@ -0,0 +1,17 @@
+//
+//  LWHNetworkHelper.h
+//  LWHNetworkHelper
+//
+//  Created by 张博文 on 2019/3/21.
+//  Copyright © 2019年 LWH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LWHNetworkHelper : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LWHNetworkHelper/LWHNetworkHelper.m
@@ -0,0 +1,13 @@
+//
+//  LWHNetworkHelper.m
+//  LWHNetworkHelper
+//
+//  Created by 张博文 on 2019/3/21.
+//  Copyright © 2019年 LWH. All rights reserved.
+//
+
+#import "LWHNetworkHelper.h"
+
+@implementation LWHNetworkHelper
+
+@end
